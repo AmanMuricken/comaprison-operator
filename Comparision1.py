f41a933a22768045ae1520eae8aab9becf28feea
AmanMuricken <89331271+AmanMuricken@users.noreply.github.com>
2021-09-04 10:46:01 +0530

    Add files via upload

---
+++ Comparision1.py
@@ -0,0 +1,11 @@
+print("I am a maximum or minimum bot")
+x = int(input("enter the first number :"))
+y = int(input("enter the second number :"))
+if(x>y):
+    print (x,"is the biggest number")
+    print (y,"is the smallest number")
+elif(x==y):
+    print ("both are equal")
+else:
+    print (y,"is the biggest number")
+    print (x,"is the smallest number")
